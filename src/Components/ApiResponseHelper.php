c5e2104d2a3035f30b1784a664c793dd6d356f61
Denys Loshakrev <denysloshkarev@gmail.com>
2020-05-02 21:21:27 +0300

    Add HTTP code to response helper

--- src/Components/ApiResponseHelper.php
+++ src/Components/ApiResponseHelper.php
@@ -11,9 +11,11 @@ class ApiResponseHelper
 {
     /**
      * @param array $data
+     * @param int   $code
+     *
      * @return JsonResponse
      */
-    public static function say($data = []){
+    public static function say($data = [], $code = 200){
         $data = ArrayMergeHelper::merge(
             [
                 'success' => Arr::pull($data, 'success', true)
@@ -22,14 +24,16 @@ class ApiResponseHelper
                 'payload' => $data
             ]
         );
-        return new JsonResponse($data);
+        return new JsonResponse($data, $code);
     }
 
     /**
      * @param array $data
+     * @param int   $code
+     *
      * @return JsonResponse
      */
-    public static function sayError($data = []){
-        return static::say(ArrayMergeHelper::merge($data, ['success' => false]));
+    public static function sayError($data = [], $code = 200){
+        return static::say(ArrayMergeHelper::merge($data, ['success' => false]), $code);
     }
 }
